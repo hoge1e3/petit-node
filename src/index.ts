bb882849c91fc699399fbef3ac167610e636eab3
hoge1e3 <hoge1e3@gmail.com>
2025-12-08 15:55:19 +0900

    to_version_1.6.2

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ export let core:Core|null=null;//=setupCore();
 declare let globalThis:any;
 //declare let global:any;
 type SFile=sfile.SFile;
-const VERSION_SRC="__VER__1.6.1__SION__";
+const VERSION_SRC="__VER__1.6.2__SION__";
 export let version=VERSION_SRC.replace(/\_\_VER\_\_/,"").replace(/\_\_SION\_\_/,"");
 function setupCore(){
     let res={
