1e4e81f19a3351e6f2d10214f1932cacb950c130
hoge1e3 <hoge1e3@gmail.com>
2025-12-08 17:57:59 +0900

    stream polyfill

--- src/NodeModule.ts
+++ src/NodeModule.ts
@@ -11,6 +11,20 @@ type PackageJson={
     type?:"module"|"commonJS",
 }
 export class NodeModule {
+    /*
+    #!run
+
+export async function main(){
+  const base="/idb/pnode-ws/node_modules/ajv-formats";
+  const codegen_1 = pNode.require(
+    "ajv/dist/compile/codegen",base);
+  console.log(codegen_1+"");
+  const e=pNode.resolveEntry("require",
+  "ajv/dist/compile/codegen",
+  base);
+  console.log("ent",e);
+  return ;
+}*/
     static parsePath(path:string):[string,string] {
         /*
         "a"      -> ["a","."]
