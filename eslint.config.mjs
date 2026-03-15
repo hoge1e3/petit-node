2b9b1b52d3c9ce78ee4abb474da326a51fdad053
hoge1e3 <hoge1e3@gmail.com>
2026-03-15 14:16:01 +0900

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,24 @@
-export default [{
-    "rules": {
-        //"no-undef": "error"
+import { defineConfig } from "eslint/config";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default defineConfig([
+  {
+    ignores: ["**/*.d.ts"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error"
     }
-}];
+  }
+]);
